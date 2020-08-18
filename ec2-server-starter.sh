0de1a5bcf925ab42950ab22ba4e0455b02a1cfef
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-08-18 21:01:27 +0530

    removed starter

--- ec2-server-starter.sh
+++
@@ -1,8 +0,0 @@
-# kill process running at 8000
-sudo kill -9 $(sudo lsof -t -i:8000)
-
-# restart mongod server
-sudo systemctl restart mongod
-
-#run node script
-/usr/local/bin/node ~/portfolio-tracking-api/backend/server.js
